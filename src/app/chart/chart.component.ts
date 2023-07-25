047a1cd59f762fa839269614a568a3f89fec62ed
richietaylor <richard@taylorfamily.co.za>
2023-07-25 15:41:48 +0200

    added first component

---
+++ src/app/chart/chart.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  standalone: true,
+  selector: 'app-chart',
+  templateUrl: './chart.component.html',
+  styleUrls: ['./chart.component.css']
+})
+export class ChartComponent {
+
+}
