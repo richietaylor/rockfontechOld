fc7f2e84307523f0b75634713d78dce28dd41aee
richietaylor <richard@taylorfamily.co.za>
2023-08-21 00:17:24 +0200

    AHHHHH HOLY SHIT IT WORKS

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -9,56 +9,54 @@
 
 import {onRequest} from "firebase-functions/v2/https";
 import * as logger from "firebase-functions/logger";
+import { google } from 'googleapis';
+import * as fs from 'fs';
 
-// export const googleAPI = onRequest((request, response) => {
-//   const express = require("express");
+import * as functions from 'firebase-functions';
 
-//   const { google } = require("googleapis");
 
-//   const app = express();
-//   const port = 443;
-//   //This allows us to parse the incoming request body as JSON
-//   app.use(express.json());
+const CREDENTIALS_PATH = '../keys.json'; // Adjust this
+// const CREDENTIALS_PATH = '../../keys/credentials.json';  // Adjust this path as needed
 
-//   // With this, we'll listen for the server on port 8080
-//   app.listen(port, () => console.log(`Listening on port ${port}`));
+export const getGoogleSheetData = functions.https.onRequest(async (req, res) => {
+    // Check for GET request
+    if (req.method !== 'GET') {
+        res.status(405).send('Method not allowed');
+        return;
+    }
 
-//   async function authSheets() {
-//       //Function for authentication object
-//       const auth = new google.auth.GoogleAuth({
-//         keyFile: "keys.json",
-//         scopes: ["https://www.googleapis.com/auth/spreadsheets"],
-//       });
-    
-//       //Create client instance for auth
-//       const authClient = await auth.getClient();
-    
-//       //Instance of the Sheets API
-//       const sheets = google.sheets({ version: "v4", auth: authClient });
-    
-//       return {
-//         auth,
-//         authClient,
-//         sheets,
-//       };
-//     }
+    // Extract query parameters
+    // const spreadsheetId = req.query.spreadsheetId as string;
+    const spreadsheetId = '1dB3Y_FnAzSqJFGSHEqWjPFuKQvlKuO60CZ7fSRcb_1o';
+    const range = req.query.range as string || 'A1:Z100';
 
-//     app.get("/", async (req: any, res: { send: (arg0: any) => void; }) => {
-//       const { sheets } = await authSheets();
-//       // console.log(req)
-//       // console.log(res)
-    
-//       // Read rows from spreadsheet
-//       const getRows = await sheets.spreadsheets.values.get({
-//         spreadsheetId: id,
-//         range: "Sheet1",
-//       });
-    
-//       res.send(getRows.data);
-//     });
-    
-//     const id = "1dB3Y_FnAzSqJFGSHEqWjPFuKQvlKuO60CZ7fSRcb_1o";
-// });
+    // Load service account credentials
+    const credentials = JSON.parse(fs.readFileSync(CREDENTIALS_PATH, 'utf8'));
+
+    // Authenticate with the service account
+    const client = new google.auth.JWT(
+        credentials.client_email,
+        undefined,
+        credentials.private_key,
+        ['https://www.googleapis.com/auth/spreadsheets.readonly'],
+    );
+
+    // Access the Sheets API
+    const sheets = google.sheets({ version: 'v4', auth: client });
+
+    try {
+        const response = await sheets.spreadsheets.values.get({
+            spreadsheetId: spreadsheetId,
+            range: range
+        });
+
+        res.send(response.data);
+    } catch (error) {
+        res.status(500).send(`Failed to fetch data from Google Sheets: ${error}`);
+    }
+});
+
+
 
 // Start writing functions
 // https://firebase.google.com/docs/functions/typescript
