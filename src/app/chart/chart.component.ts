fdfba838dc188ce920a29e944485850fc3d9ed05
richietaylor <richard@taylorfamily.co.za>
2023-12-21 15:49:28 +0200

    Mess

--- src/app/chart/chart.component.ts
+++ src/app/chart/chart.component.ts
@@ -1,22 +1,22 @@
-import { Component, Input, OnInit } from '@angular/core';
+// import { Component, Input, OnInit } from '@angular/core';
 
-@Component({
-  selector: 'app-chart',
-  templateUrl: './chart.component.html',
-  styleUrls: ['./chart.component.css']
-})
-export class ChartComponent {
-  @Input() data: Entry[];
+// @Component({
+//   selector: 'app-chart',
+//   templateUrl: './chart.component.html',
+//   styleUrls: ['./chart.component.css']
+// })
+// export class ChartComponent {
+//   @Input() data: Entry[];
 
-  // Transform your data into a format suitable for ngx-charts
-  chartData: any[];
+//   // Transform your data into a format suitable for ngx-charts
+//   chartData: any[];
 
-  ngOnChanges() {
-    this.chartData = this.transformData(this.data);
-  }
+//   ngOnChanges() {
+//     this.chartData = this.transformData(this.data);
+//   }
 
-  transformData(data: Entry[]): any[] {
-    // Transform logic here
-    return transformedData;
-  }
-}
+//   transformData(data: Entry[]): any[] {
+//     // Transform logic here
+//     return transformedData;
+//   }
+// }
